c27b40d72f6bec358a9a4ac1fcf3eccc43e60b3a
John Baylor <john.baylor@gmail.com>
2025-03-30 17:18:08 -0700

    Moved over most of Pentomino module from "tiling_game"; added initial tests

--- lib/pentomino.ex
+++ lib/pentomino.ex
@@ -1,18 +1,163 @@
 defmodule Pentomino do
   @moduledoc """
-  Documentation for `Pentomino`.
+    Pentomino is what we call the shape of a piece in Blokus, and similar games.
+    These pieces are the sum of the set of pentominos, tetrominos, trominos,
+    and the domino and monomino - but we'll call it by its biggest shapes.
+    `https://en.wikipedia.org/wiki/Polyomino`
+    
+    Each pentomino square is represented as a grid position in a 5x5 grid.
+    However, the 5x5 grid has been flattened to an array, with positions
+    represented by a single integer in the range 0..24
+    
   """
+
+  @max_dimension 5
+  @pentominos [
+    # monomio or dot
+    [0],
+    # domino or n-dash
+    [0, 1],
+    # m-dash or straight-3
+    [0, 1, 2],
+    # small angle
+    [0, 1, 5],
+    # straight-4
+    [0, 1, 2, 3],
+    # short L
+    [0, 1, 2, 5],
+    # short T
+    [0, 1, 2, 6],
+    # square
+    [0, 1, 5, 6],
+    # squiggle
+    [1, 2, 5, 6],
+    # see image at https://gp.home.xs4all.nl/Pieces.GIF
+    # F
+    [1, 2, 5, 6, 11],
+    # I or straight-5
+    [0, 1, 2, 3, 4],
+    # L
+    [0, 1, 2, 3, 5],
+    # N
+    [0, 1, 2, 7, 8],
+    # P
+    [0, 1, 2, 5, 6],
+    # big T
+    [0, 1, 2, 6, 11],
+    # U
+    [0, 1, 2, 5, 7],
+    # V, or big angle
+    [0, 1, 2, 5, 10],
+    # W
+    [1, 2, 5, 6, 10],
+    # X
+    [1, 5, 6, 7, 11],
+    # Y
+    [0, 1, 2, 3, 6],
+    # Z
+    [1, 2, 6, 10, 11]
+  ]
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Pentomino.hello()
-      :world
-
+  List all the pentominos, with no rotations or flips.
   """
-  def hello do
-    :world
+  def all do
+    @pentominos
+  end
+
+  @doc """
+  One particular piece, indexed from 0 to 20.
+  Returns nil for out-of-bounds index.
+  """
+  def piece(index) do
+    all() |> Enum.fetch(index)
+  end
+
+  @doc """
+  Convert from an index to the X offset.
+  """
+  def x_offset(index), do: Integer.mod(index, @max_dimension)
+
+  @doc """
+  Convert from an index to the Y offset.
+  """
+  def y_offset(index), do: Integer.floor_div(index, @max_dimension)
+
+  @doc """
+  Convert from an index to an `[X,Y]` list.
+  """
+  def index_to_xy(index) do
+    [_x, _y] = [x_offset(index), y_offset(index)]
+  end
+
+  @doc """
+  Convert from an `[X,Y]` list to the corresponding index.
+  """
+  def xy_to_index([x, y], width \\ @max_dimension) do
+    x + width * y
+  end
+
+  # flipping and rotating may move the piece away from the (1,1) origin
+  # so this should snug it up and to the left.
+  @doc false
+  def snug(pentomino) do
+    min_x = pentomino |> Enum.map(fn idx -> x_offset(idx) end) |> Enum.min()
+    min_y = pentomino |> Enum.map(fn idx -> y_offset(idx) end) |> Enum.min()
+    offset = min_x + min_y * @max_dimension
+    IO.inspect([min_x, min_y, offset])
+
+    pentomino |> Enum.map(fn idx -> idx - offset end)
+  end
+
+  @doc """
+  Flip a piece (list of indexes) to a new set of indexes that
+  has been "snugged up" to the top-left corner.
+  """
+  def flip_top_to_bottom(pentomino) do
+    Enum.map(pentomino, fn index ->
+      [x, y] = index_to_xy(index)
+      xy_to_index([x, @max_dimension - 1 - y])
+    end)
+    |> snug()
+  end
+
+  @doc """
+  Flip a piece (list of indexes) to a new set of indexes that
+  has been "snugged up" to the top-left corner.
+  """
+  def flip_side_to_side(pentomino) do
+    Enum.map(pentomino, fn index ->
+      [x, y] = index_to_xy(index)
+      xy_to_index([@max_dimension - 1 - x, y])
+    end)
+    |> snug()
+  end
+
+  @doc """
+  Rotate a piece (list of indexes) to a new set of indexes that
+  has been "snugged up" to the top-left corner.
+  """
+  def rotate_left(pentomino) do
+    core_rotate_left(pentomino) |> snug()
+  end
+
+  @doc false
+  defp core_rotate_left(pentomino) do
+    Enum.map(pentomino, fn index ->
+      [x, y] = index_to_xy(index)
+      xy_to_index([y, @max_dimension - 1 - x])
+    end)
+  end
+
+  @doc """
+  Rotate a piece (list of indexes) to a new set of indexes that
+  has been "snugged up" to the top-left corner.
+  """
+  def rotate_right(pentomino) do
+    pentomino
+    |> core_rotate_left
+    |> core_rotate_left
+    |> core_rotate_left
+    |> snug()
   end
 end
--- test/pentomino_test.exs
+++ test/pentomino_test.exs
@@ -2,7 +2,27 @@ defmodule PentominoTest do
   use ExUnit.Case
   doctest Pentomino
 
-  test "greets the world" do
-    assert Pentomino.hello() == :world
+  describe "all/0" do
+    test "returns entire list" do
+      all = Pentomino.all()
+      assert Enum.count(all) == 21
+    end
+  end
+
+  describe "piece/1" do
+    test "returns the list of cells for the piece" do
+      assert Pentomino.piece(0) == {:ok, [0]}
+      assert Pentomino.piece(1) == {:ok, [0, 1]}
+      assert Pentomino.piece(9) == {:ok, [1, 2, 5, 6, 11]}
+    end
+
+    test "returns end-of-list values for negative piece number" do
+      assert Pentomino.piece(-1) == {:ok, [1, 2, 6, 10, 11]}
+      assert Pentomino.piece(-2) == {:ok, [0, 1, 2, 3, 6]}
+    end
+
+    test "returns nil for too-large piece number" do
+      assert Pentomino.piece(22) == :error
+    end
   end
 end
